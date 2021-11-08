e10ed290306c246cc283c87d05c9896e678f63b6
PiotrIwanek <piotr.iwanek@familiamed.pl>
2021-11-08 21:47:48 +0100

    change host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  URL: "http://192.168.1.16:4200/api/v1/",
+  URL: "http://10.10.8.253:4200/api/v1/",
   // SpringServerIP: "http://10.10.8.253:8090/"
 };
 
